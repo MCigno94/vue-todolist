fe9b102542aefb3444d95fdf10c02e419f6bd879
MCigno94 <mattiacigno27@gmail.com>
2022-04-23 08:54:19 +0200

    first Bonus extra completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,13 @@ MILESTONE 3
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
+## Bonus-extra (opzionale)
+- Quando una task é stata completa allora l'utente vuole che venga inserita in un'altra colonna tipo "tasks completate"
+- se una task é stata marcata come completa per sbaglio allora vuole poterla rimettere nella todo list (cliccando su un altra icona)
+- ah non é finita, dice che quando cancella una task non vuole che questa venga subito rimossa, ma vuole che resti visibile ma venga spostata in una colonna tipo "cestino"
+- si, l'utente é un rompi scatole, dice infine che vuole poter rimuovere tutte le tasks nel cestino cliccando su un pulsante tipo "svuota cestino"
+Il nostro utente per ora sembra non avere altre richieste ... ma chissá se dopo gli viene in mente che vuole anche poter rimettere una task cancellata nella lista di tasks da fare, magari l'ha cancellata per sbaglio...
 */
 
 const app = new Vue({
@@ -30,16 +37,19 @@ const app = new Vue({
                 text: 'Fare il bucato',
                 done: false
             },
-        ]
+        ],
+        completedTasks: []
     },
     methods: {
-        removeTask(index) {
-            this.tasks.splice(index, 1)
-            console.log(this.tasks.length);
+        removeTask(index, task) {
+            this.tasks.splice(index, 1);
+            //console.log(this.tasks.length);
+            this.completedTasks.unshift(task);
+            //console.log(this.tasksCompleted);
         },
         addTask() {
             //console.log(this.inputText);
-            if (this.inputText != '') {
+            if (this.inputText.length > 2) {
                 newTask = {
                     text: this.inputText,
                     done: false
@@ -48,7 +58,6 @@ const app = new Vue({
             }
             //console.log(newTask);
             this.inputText = '';
-            console.log('');
         },
         strikethroughTask(index) {
             //console.log(this.tasks[index].done);
@@ -62,3 +71,9 @@ const app = new Vue({
 
     }
 });
+
+// aggiungo l'icona cestino per eliminare e spostare la task nel "cestino" - ok
+// aggiungo l'icona check per barrare la task completata (al posto di cliccare sulla scritta) - ok
+// creo un nuovo array (task completate) - ok
+// appena cancello una task la pusho nel nuovo array - ok
+// se il nuovo array contiene elementi lo visualizzo a schermo
